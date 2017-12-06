90036f94dc46f49e3107dc91fbff1425c1d1b654
Shangoman <abdulbaruwa@hotmail.com>
2017-12-06 17:51:53 +0000

    Added Server Project structure with base tests and models

---
+++ Baml.Weather.Web.Tests/Units/FetchManagerTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace Baml.Weather.Web.Tests.Units
+{
+    public class FetchManagerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        public void FetchAndSyncWeather_ReturnsCachedData_WhenNonExpiredOneExists()
+        {
+
+
+
+        }
+
+        public void FetchAndSyncWeather_ReturnsCachedDataIfAny_WhenThirdPartyCallFails()
+        {
+
+        }
+
+        public void FetchAndSynceWeather_ReturnsNewData_WhenCachedOneHasExpired()
+        {
+
+        }
+
+        public void FetchAndSyncWeather_StoresNewDataInCache_WhenNoneExistsInCache()
+        {
+
+
+        }
+
+        public void FetchAndSyncWeather_ReplacesDataInCache_WhenExistingOneHasExpired()
+        {
+
+        }
+
+    }
+}
---
+++ server/Baml.Weather.Web/Core/Dtos/TimedWeatherDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Baml.Weather.Web.Core.Dtos
+{
+    public class TimedWeatherDetail
+    {
+        public string Day { get; set; }
+        public double Temperature { get; set; }
+        public double Wind { get; set; }
+        public double WindDirection { get; set; }
+        public int Humidity { get; set; }
+        public string ShortDescription { get; set; }
+        public DateTimeOffset DayTime { get; set; }
+        public double Precipitation { get; set; }
+        public int Hour => DayTime.Hour;
+    }
+}
